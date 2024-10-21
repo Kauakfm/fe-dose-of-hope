361cc230d07f675b84f3a41cd15884fe34ab8282
pimentazil <1matheuspimentel@gmail.com>
2024-10-20 23:09:56 -0300

    modal para aprovar medicamento

--- doacaoMedicamento/src/paginas/Administrativo/Aprovar/index.js
+++ doacaoMedicamento/src/paginas/Administrativo/Aprovar/index.js
@@ -4,12 +4,14 @@ import api from "../../../services/api";
 import Header from "../../../componentes/Header";
 import { formatarData } from "../../../utils/utils";
 import Columns from "../../../componentes/Table2/Columns";
-import './aprovar.css'
-import { style } from "@mui/system";
+import './aprovar.css';
+import Logo from '../../../imagens/doselogo2.png'
 
 export default function Aprovar() {
     const [data, setData] = useState([]);
     const [visibleColumns, setVisibleColumns] = useState(["nomeUsuario", "tipoProdutoDescricao", "nome", "validade"]);
+    const [isModalVisible, setIsModalVisible] = useState(false); // Estado para controlar a visibilidade do modal
+    const [selectedItem, setSelectedItem] = useState(null); // Estado para armazenar o item selecionado
 
     useEffect(() => {
         const handleObterDoacoes = async () => {
@@ -17,7 +19,7 @@ export default function Aprovar() {
                 const response = await api.get('Produto');
                 if (response.status === 200) {
                     const json = response.data.produtos;
-                    console.log(json)
+                    console.log(json);
                     setData(json.map(e => ({
                         ...e,
                         validade: formatarData(e.validade)
@@ -33,28 +35,65 @@ export default function Aprovar() {
 
     const handleEdit = (item) => {
         console.log("Edit", item.codigo);
+        setSelectedItem(item);
+        setIsModalVisible(true);
     };
 
-    const handleDelete = (item) => {
-        console.log("Delete", item);
+    const handleCloseModal = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleAction = () => {
+        console.log("Aprovar/Reprovar:", selectedItem);
+        handleCloseModal();
     };
 
     const actionButtons = [
-        { label: "Editar", action: handleEdit, className: "btn btn-customEditar" },
-        { label: "Excluir", action: handleDelete, className: "btn btn-customDelete" }
+        { label: "Aprovar", action: handleEdit, className: "btn btn-customEditar" },
     ];
 
     return (
         <div className="body-administrativo">
             <main>
                 <div className="table-container">
-                    <Table2 botoesDeAcao={actionButtons} dados={data} colunasVisiveis={visibleColumns} numeroDePagina={20} >
+                    <Table2 botoesDeAcao={actionButtons} dados={data} colunasVisiveis={visibleColumns} numeroDePagina={20}>
                         <Columns header={"nomeUsuario"} title={"Nome usuário"} />
                         <Columns header={"tipoProdutoDescricao"} title={"Tipo produto descrição"} />
                         <Columns header={"nome"} title={"Nome remédio"} />
                         <Columns header={"validade"} title={"Validade"} />
                     </Table2>
                 </div>
+
+                {isModalVisible && (
+                    <div className="modal-incricao-presencial modal fade show" style={{ display: 'block' }} id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content" style={{ backgroundColor: 'transparent', padding: '20px', marginTop: '-20%'}}>
+                                <div className="modal-body">
+                                    <img className="logo3" src={Logo} alt="LogoDose" style={{ width: '50%', marginBottom: '10%' }} />
+                                    <h5 style={{ textAlign: "center" }}>Preencha os campos para aprovar/reprovar!</h5>
+                                    <form style={{ display: "flex", flexDirection: "column", width: "20vw" }}>
+                                        <div className="mb-7" style={{ display: "flex", flexDirection: "column", width: "20vw", alignItems: "start" }}>
+                                            <label className="col-form-label">Aprovar / Reprovar:</label>
+                                            <select className="form-control">
+                                                <option value="0">Nenhuma opção selecionada</option>
+                                                <option value="1">Aprovar</option>
+                                                <option value="3">Reprovar</option>
+                                            </select>
+                                        </div>
+                                        <div className="mb-7" style={{ display: "flex", flexDirection: "column", width: "20vw", alignItems: "start" }}>
+                                            <label className="col-form-label">Observação:</label>
+                                            <input type="text" className="form-control" />
+                                        </div>
+                                    </form>
+                                    <div style={{ marginTop: "20px" }}>
+                                        <button type="button" className="btn btn-danger" onClick={handleCloseModal} style={{ marginRight: "8px" }}>Sair</button>
+                                        <button type="button" className="btn btn-success" onClick={handleAction} style={{ marginLeft: "8px" }}>Acionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </main>
         </div>
     );
